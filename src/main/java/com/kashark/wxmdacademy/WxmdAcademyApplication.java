ab3265a938c2c10462948c2365eaf28a0f4aef4d
DESKTOP-KOTFTQT\dell <1146838865@qq.com>
2024-08-03 17:36:54 +0800

    文件创建

---
+++ src/main/java/com/kashark/wxmdacademy/WxmdAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.kashark.wxmdacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxmdAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxmdAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kashark/wxmdacademy/WxmdAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashark.wxmdacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxmdAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
